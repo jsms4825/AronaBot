21612cd347c728865332148f3d5a82c9631aaed3
jsms4825 <jsms4825@naver.com>
2024-02-16 21:58:17 +0900

    making party pdf

--- TotalAssault/makeAssaultParty.js
+++ TotalAssault/makeAssaultParty.js
@@ -1,16 +1,24 @@
 const fs = require('node:fs');
 const pdfDocument = require('pdfkit');
+const firstPos = 30;
+
+let Xpos = firstPos;
+let Ypos = firstPos;
+let numOfParties = 1;
+
+const A4_X = 841.89;
+const A4_Y = 595.28;
+
+    // create PDF file
 
     const boss = require('./datas/bossParty.json');
-
-    const doc = new pdfDocument({size: "A5",font: "font/Giants-Regular.otf", layout: 'landscape',
+    const doc = new pdfDocument({size: "A4", font: "font/Giants-Regular.otf", layout: 'landscape',
                                 margins: {
                                     top: 30,
                                     left: 30,
                                     right: 30,
                                     bottom: 30
                                 }});
-
     const ExportedFilePath = `./${boss[0].name}_party.pdf`;
 
     if (fs.existsSync(ExportedFilePath))
@@ -18,48 +26,102 @@ const pdfDocument = require('pdfkit');
 
     doc.pipe(fs.createWriteStream(ExportedFilePath));
 
-    const firstPos = 30;
-
-    let Xpos = firstPos;
-    let Ypos = firstPos;
-    let numOfParties = 1;
-
     // INSANE party
 
-    doc.fontSize(20)
-        .text(`${boss[0].name} 인세인 조합`, Xpos, Ypos);
-    
-    Ypos += 50;
+    doc.opacity(0.25)
+        .image(`./Images/Boss/${boss[0].name}.jpeg`, -90, 25, {height: A4_Y - 50});
+    doc.opacity(1);
+    doc.image('./Images/Blue_Archive.svg.png', 650, 520, {scale: 0.35})
+    doc.polygon([0, 15], [500, 15], [450, 70], [0, 70])
+        .fill("#098bfa");
+    doc.stroke();
+
+    doc.fontSize(32)
+        .fillColor("#272727")
+        .text(`${boss[0].name} 인세인 조합`, Xpos, Ypos - 10);
+    Ypos += 80;
     
     for(const parties of boss[0].party_insane) {
 
-        doc.fontSize(16)
-            .text(`${numOfParties++} 파티`);
+        doc.fontSize(20)
+            .text(`${numOfParties++} 파티`, Xpos, Ypos);
             
-        Ypos += 10;
+        Ypos += 30;
         
         for(let i=0; i<6; i++) {
-            doc.image(`./Images/Students/Face/${parties[i]}.jpg`, Xpos, Ypos, {width: 80, height: 70})
-                .fontSize(12)
-                .text(`${parties[i]}`, Xpos, Ypos + 70, {width: 80, align: 'center'});
-            Xpos += 90;
+            doc.image(`./Images/Students/Face/${parties[i]}.jpg`, Xpos, Ypos, {width: 120, height: 100})
+                .fontSize(16)
+                .text(`${parties[i]}`, Xpos, Ypos + 100, {width: 120, align: 'center'});
+            Xpos += 130;
         }
 
         Xpos = firstPos;
-        Ypos += 90;
+        Ypos += 140;
+
+        if(Ypos + 180 >= A4_Y) {
+            addPage(doc);
+            Ypos = firstPos;
+            doc.fontSize(32)
+                .fillColor("#272727")
+                .text(`${boss[0].name} 인세인 조합`, Xpos, Ypos - 10);
+            Ypos += 80;
+        }
     }
 
-    doc.fontSize(14)
-        .text(`${boss[0].coment_insane}`, Xpos, Ypos + 30);
+    doc.fontSize(16)
+        .text(`${boss[0].coment_insane}`, Xpos, Ypos);
 
     // TORMENT party
 
-    doc.addPage();
-
-    Xpos = firstPos; 
+    addPage(doc);
     Ypos = firstPos;
+    numOfParties = 1;
 
-    doc.fontSize(18)
-        .text(`${boss[0].name} 토먼트 조합`, Xpos, Ypos);
+    doc.fontSize(32)
+        .fillColor("#272727")
+        .text(`${boss[0].name} 토먼트 조합`, Xpos, Ypos - 10);
+    Ypos += 80;
+
+    for(const parties of boss[0].party_torment) {
+
+        doc.fontSize(20)
+            .text(`${numOfParties++} 파티`, Xpos, Ypos);
+            
+        Ypos += 30;
+        
+        for(let i=0; i<6; i++) {
+            doc.image(`./Images/Students/Face/${parties[i]}.jpg`, Xpos, Ypos, {width: 120, height: 100})
+                .fontSize(16)
+                .text(`${parties[i]}`, Xpos, Ypos + 100, {width: 120, align: 'center'});
+            Xpos += 130;
+        }
+
+        Xpos = firstPos;
+        Ypos += 140;
+
+        if(Ypos + 180 >= A4_Y) {
+            addPage(doc);
+            Ypos = firstPos;
+            doc.fontSize(32)
+                .fillColor("#272727")
+                .text(`${boss[0].name} 토먼트 조합`, Xpos, Ypos - 10);
+            Ypos += 80;
+        }
+    }
+
+    doc.fontSize(16)
+        .text(`${boss[0].coment_torment}`, Xpos, Ypos);
 
     doc.end();
+
+function addPage(doc) {
+    doc.addPage();
+    doc.opacity(0.25)
+        .image(`./Images/Boss/${boss[0].name}.jpeg`, -90, 25, {height: A4_Y - 50});
+    doc.opacity(1);
+    doc.image('./Images/Blue_Archive.svg.png', 650, 520, {scale: 0.35});
+    doc.polygon([0, 15], [500, 15], [450, 70], [0, 70])
+        .fill("#098bfa");
+    doc.stroke();
+    doc.fill('black');
+}
